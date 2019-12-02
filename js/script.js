dd3ea30c0b8603e81c64daf8941dd29130a60ce0
Lukasz <34094307+devluke88@users.noreply.github.com>
2019-12-02 23:14:24 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
-    $('#slides').superslides({
-        animation: 'fade',
-        play: 4000,
-        pagination: false
-    });
+    // $('#slides').superslides({
+    //     animation: 'fade',
+    //     play: 4000,
+    //     pagination: false
+    // });
 
     $("#navigation li a").click(function(e) {
         var targetElement = $(this).attr("href");
